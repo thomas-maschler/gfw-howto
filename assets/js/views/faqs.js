bae46854e2d0dbf828273295667b7fa2f8628e44
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2016-02-09 17:44:16 +0100

    Update 2016-02-09T16:44:09.415Z

--- assets/js/views/faqs.js
+++ assets/js/views/faqs.js
@@ -15,29 +15,97 @@
       'click .toggle' : 'toggleFaq'
     },
 
-    model: new (Backbone.Model.extend({})),
+    collection: new (Backbone.Collection.extend({
+
+      url: baseurl + '/json/faqs.json',
+
+      getGroups: function(filter,page) {
+        this.filter = filter;
+        if(!!this.filter && this.filter != 'all') {
+          this.collection = _.groupBy(_.sortBy(this.toJSON(), 'order'), 'app');
+          return _.compact(_.map(this.collection, function(group, k) {
+            if (k != this.filter) { return null; }
+            return _.sortBy(group,'order').slice(page*5, (page*5) + 5);
+          }.bind(this)));
+        } else {
+          this.collection = _.flatten(_.map(_.groupBy(_.sortBy(this.toJSON(), 'order'), 'app'), function(group){
+            return group
+          }.bind(this))).slice(page*5, (page*5) + 5);
+          return _.groupBy(this.collection, 'app');
+        }
+      },
+
+      getCount: function() {
+        if (!!this.filter && this.filter != 'all') {
+          var filtered = _.groupBy(this.toJSON(), 'app')[this.filter];
+          return (!!filtered) ? filtered.length : 0;
+        } else {
+          return this.toJSON().length;
+        }
+      }
+
+    })),
+
+    template: HandlebarsTemplates['faqs'],
 
     initialize: function(settings) {
       var opts = settings && settings.options ? settings.options : {};
       this.options = _.extend({}, this.defaults, opts);
-      this.model.set(this.options.model);
-      this.cache();
+      this.collection.fetch().done(function() {
+        this.setListeners();
+        this.render('all',0)
+      }.bind(this));
+    },
+
+    setListeners: function() {
+      Backbone.Events.on('faqs/filter', function(value) {
+        this.render(value, 0);
+      }.bind(this));
     },
 
     cache: function() {
-      this.$listItems = $('.m-faqs-list li');
+      this.$listItems = this.$el.find('.m-faqs-list li');
+      this.$paginationContainer = this.$el.find('.m-faqs-pagination');
     },
 
+    render: function(filter,page) {
+      this.$el.html(this.template({
+        groups: this.collection.getGroups(filter,page)
+      }));
+      this.cache();
+      this.initPaginate(filter,page);
+    },
+
+    initPaginate: function(filter,page){
+      // pagination
+      this.$paginationContainer.pagination({
+        items: this.collection.getCount(),
+        itemsOnPage: 5,
+        currentPage: page + 1,
+        displayedPages: 3,
+        selectOnClick: false,
+        prevText: ' ',
+        nextText: ' ',
+        onPageClick: _.bind(function(page, e){
+          e && e.preventDefault();
+          this.render(filter,page-1)
+          this.$paginationContainer.pagination('drawPage', page);
+        }, this )
+      });
+    },
+
+    // Events
     toggleFaq: function(e) {
       var $parent = $(e.currentTarget).parent();
-
       if ($parent.hasClass('-selected')) {
         this.$listItems.removeClass('-selected');
       } else {
         this.$listItems.removeClass('-selected');
         $parent.addClass('-selected');
       }
-    }
+    },
+
+
 
   });
 
